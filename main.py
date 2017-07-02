eb0ae05dc86924a061117e93005171012e82ebc6
Kevin Tam <tam.kevin@hotmail.com>
2017-07-02 12:17:59 -0700

    add configuration with sqlalchemy

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy 
+# Note: the connection string after :// contains the following info:
+# user:password@server:portNumber/databaseName
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
