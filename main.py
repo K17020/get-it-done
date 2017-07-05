81b36f268ead11d9d17c14d930d1bb47bace736b
Kevin Tam <tam.kevin@hotmail.com>
2017-07-04 19:24:27 -0700

    refactor, changed how the data is represented on html page

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductiv
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
+
+# This class creates a table in the database get-it-done
 class Task(db.Model):
     
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Colum(db.Boolean)
+    id = db.Column(db.Integer, primary_key=True) # Creates the primary key in a table
+    name = db.Column(db.String(120)) # Creates a name that is a string that can only be 120 characters
+    completed = db.Column(db.Boolean) # Creates a bool for if the task has been completed
 
     def __init__(self, name):
         self.name = name
@@ -23,23 +25,28 @@ class Task(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
+# This takes the users input from the todo.html and adds it to the database. 
     if request.method == 'POST':
         task_name = request.form['task']
         new_task = Task(task_name)
         db.session.add(new_task)
         db.session.commit()
 
-    tasks = Task.query.all()
-    return render_template('todo.html',title="Get It Done!", tasks=tasks)
+    tasks = Task.query.filter_by(completed=False).all() # Checks to see if the task in the database is True or False and removes it from the page
+    completed_tasks = Task.query.filter_by(completed=True).all() # See above 
+    return render_template('todo.html', title="Get It Done!",  # repost data from the database according to if it has been completed or not
+        tasks=tasks, completed_tasks=completed_tasks)
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
-    
+
     task_id = int(request.form['task-id'])
     task = Task.query.get(task_id)
-    db.session.delete(task)
+    task.completed = True
+    db.session.add(task)
     db.session.commit()
 
     return redirect('/')
+
 if __name__ == '__main__':
     app.run()
